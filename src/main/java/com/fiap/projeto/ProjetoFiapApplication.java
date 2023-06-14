e721dd02cc1542d1f618b6d26eb21c4ab71e6262
Richard Cardoso <richard.166@hotmail.com>
2023-06-13 22:07:33 -0300

    chore: first commit

---
+++ src/main/java/com/fiap/projeto/ProjetoFiapApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFiapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/projeto/ProjetoFiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
