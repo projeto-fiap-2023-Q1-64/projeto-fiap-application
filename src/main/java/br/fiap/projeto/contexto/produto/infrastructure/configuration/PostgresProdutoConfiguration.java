e23f11cd75c3846380e07134d12429f2b8b8fda2
Mario Sclavo <mmsclavo@gmail.com>
2023-06-19 22:45:51 -0300

    refactor: ajuste de nome para adequar ao contexto

--- src/main/java/br/fiap/projeto/contexto/produto/infrastructure/configuration/PostgresProdutoConfiguration.java
+++ src/main/java/br/fiap/projeto/contexto/produto/infrastructure/configuration/PostgresProdutoConfiguration.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories(basePackages = "br.fiap.projeto.contexto.produto.infrastructure")
 @EntityScan("br.fiap.projeto.contexto.produto.infrastructure.entity")
-public class PostgresConfiguration {
+public class PostgresProdutoConfiguration {
 }
