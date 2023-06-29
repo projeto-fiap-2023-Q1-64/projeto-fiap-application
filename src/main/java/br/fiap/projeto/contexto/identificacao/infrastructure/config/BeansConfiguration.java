5135f94168c4c3ad6720fe6c39c4702788c96c29
Mario Sclavo <mmsclavo@gmail.com>
2023-06-28 21:02:02 -0300

    fix: BeanConfiguration identificação e produto

--- src/main/java/br/fiap/projeto/contexto/identificacao/infrastructure/config/BeansConfiguration.java
+++ src/main/java/br/fiap/projeto/contexto/identificacao/infrastructure/config/BeansConfiguration.java
@@ -1,8 +1,15 @@
 package br.fiap.projeto.contexto.identificacao.infrastructure.config;
 
+import br.fiap.projeto.contexto.identificacao.domain.port.repository.ClienteRepository;
+import br.fiap.projeto.contexto.identificacao.domain.port.service.ClienteService;
+import br.fiap.projeto.contexto.identificacao.domain.service.ClienteServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeansConfiguration {
-
+    @Bean
+    public ClienteService clienteService(ClienteRepository clienteRepository) {
+        return new ClienteServiceImpl(clienteRepository);
+    }
 }
