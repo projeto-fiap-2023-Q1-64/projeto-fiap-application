6b7a21ba0829c6c04782deb446d146be549f8243
danluri <danluri@gmail.com>
2023-09-02 21:29:31 -0300

    refactor: codigo-comanda->codigo-pedido

--- src/main/java/br/fiap/projeto/contexto/comanda/external/api/FinalizarComandaApiExternal.java
+++ src/main/java/br/fiap/projeto/contexto/comanda/external/api/FinalizarComandaApiExternal.java
@@ -1,18 +1,20 @@
 package br.fiap.projeto.contexto.comanda.external.api;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.fiap.projeto.contexto.comanda.adapter.controller.port.IAtualizaComandaControllerAdapter;
 import br.fiap.projeto.contexto.comanda.adapter.controller.rest.dto.ComandaDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/comandas")
@@ -29,7 +31,7 @@ public class FinalizarComandaApiExternal {
     @PatchMapping("/{codigo-pedido}/finalizar")
     @ResponseBody
     @ApiOperation(value = "Finaliza uma comanda", notes = "Este endpoint informa a finalização da comanda")
-    ResponseEntity<ComandaDTO> finalizar(@PathVariable("codigo-pedido") UUID codigoComando)
+    ResponseEntity<ComandaDTO> finalizar(@PathVariable("codigo-pedido") UUID codigoPedido)
             throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(this.finalizaComandaPortControllerAdapter.atualizaStatusComanda(codigoPedido));
